e7d19fa1cd8eed55464af7eae8d1c304b5479749
JudahWilson <judah.d.wilson@gmail.com>
2016-11-23 15:37:03 -0600

    an aspx file and its codebehind file for a simple ordering process prototype

---
+++ Order.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Judah.aspx.cs" Inherits="Judah" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Start Order</h1>
+        First Name:
+        <asp:TextBox ID="inpFName" runat="server" style="margin-top: 0px" Height="16px" Width="122px"></asp:TextBox>
+        
+        
+
+        &nbsp;&nbsp;&nbsp;&nbsp; Last Name:&nbsp;&nbsp;
+        <asp:TextBox ID="inpLName" runat="server" style="margin-top: 0px" Height="16px" Width="122px"></asp:TextBox>
+        
+        
+
+        <br />
+        <br />
+        
+        
+
+        <asp:RadioButton id="inpPickup" runat="server" GroupName="inpOrderOrDelivery" text="Pickup"/>
+        <asp:RadioButton id="inpDelivery" runat="server" GroupName="inpOrderOrDelivery" Text="Delivery"/>
+
+        <br />
+        <asp:DropDownList ID="inpHour" runat="server">
+            <asp:ListItem Selected="True" Value="1"></asp:ListItem>
+            <asp:ListItem Value="2"></asp:ListItem>
+            <asp:ListItem Value="3"></asp:ListItem>
+            <asp:ListItem Value="4"></asp:ListItem>
+            <asp:ListItem Value="5"></asp:ListItem>
+            <asp:ListItem Value="6"></asp:ListItem>
+            <asp:ListItem Value="7"></asp:ListItem>
+            <asp:ListItem Value="8"></asp:ListItem>
+            <asp:ListItem Value="9"></asp:ListItem>
+            <asp:ListItem Value="10"></asp:ListItem>
+            <asp:ListItem Value="11"></asp:ListItem>
+            <asp:ListItem Value="12"></asp:ListItem>
+        </asp:DropDownList>
+        :<asp:DropDownList ID="inpMinute" runat="server">
+            <asp:ListItem Value="00"></asp:ListItem>
+            <asp:ListItem Value="15"></asp:ListItem>
+            <asp:ListItem Value="30"></asp:ListItem>
+            <asp:ListItem Value="45"></asp:ListItem>
+        </asp:DropDownList>
+&nbsp;&nbsp;
+        <asp:DropDownList ID="inpMeridian" runat="server">
+            <asp:ListItem Selected="True" Value="AM">AM</asp:ListItem>
+            <asp:ListItem Value="PM">PM</asp:ListItem>
+        </asp:DropDownList>
+
+        <br />
+
+        <asp:Button id="inpSubmit" runat="server" OnClick="S_Clicked" Text="Submit" />
+
+
+        <br />
+        <br />
+        <p>
+
+
+
+        <asp:TextBox ID="results" runat="server" Height="345px" Width="269px" TextMode="MultiLine"></asp:TextBox>
+
+
+
+        </p>
+
+
+
+    </form>
+</body>
+</html>
---
+++ Order.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Judah : System.Web.UI.Page
+{
+    bool PICKUP = false,
+         DELIVERY = true;
+
+    Order myOrder = new Order();
+
+    public class Order
+    {
+        private String fName;
+        public String FName
+        {
+            get
+            {
+                return this.fName;
+            }
+            set
+            {
+                this.fName = value;
+            }
+        }
+
+        private String lName;
+        public String LName
+        {
+            get
+            {
+                return this.lName;
+            }
+            set
+            {
+                this.lName = value;
+            }
+        }
+
+        private bool order_type;
+        public bool Order_type
+        {
+            get
+            {
+                return this.order_type;
+            }
+            set
+            {
+                this.order_type = value;
+            }
+        }
+
+        private String receive_order_time;
+        public String Receive_order_time
+        {
+            get
+            {
+                return this.receive_order_time;
+            }
+            set
+            {
+                this.receive_order_time = value;
+            }
+        }
+    }
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    public void S_Clicked(object sender, EventArgs e)
+    {
+        // Assign customer first and last name to the order
+        myOrder.FName = inpFName.Text;
+        myOrder.LName = inpLName.Text;
+
+        // Determine method of receiving order
+        if (inpPickup.Checked == true)
+        {
+            myOrder.Order_type = PICKUP;
+        }
+        else if (inpDelivery.Checked == true)
+        {
+            myOrder.Order_type = DELIVERY;
+        }
+
+        // Assign a time for the order
+        myOrder.Receive_order_time = inpHour.SelectedValue + ":" + 
+                                     inpMinute.SelectedValue + " " + 
+                                     inpMeridian.SelectedValue;
+
+        // Display information about the order
+        displayResults(myOrder);
+    }
+
+    void displayResults(Order myOrder)
+    {
+        results.Text += "Customer Name: " + myOrder.LName + ", " 
+                      + myOrder.FName;
+
+        results.Text += "\nMethod of receiving order: ";
+        if (myOrder.Order_type == PICKUP)
+            results.Text += "Pickup ";
+        else if (myOrder.Order_type == DELIVERY)
+            results.Text += "Delivery ";
+        else
+            results.Text += "Not Specified";
+
+        results.Text += "\nDeliveryTime: " + myOrder.Receive_order_time;
+    }
+}
